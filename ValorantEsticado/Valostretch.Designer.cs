7f61a8d68a02d3e95aaedca0fbd3d7a1e787a750
kayo <85133224+realkayo@users.noreply.github.com>
2024-02-29 19:53:09 +0000

    Add files via upload

---
+++ ValorantEsticado/Valostretch.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ValorantEsticado
+{
+    partial class Valostretch
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Valostretch));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.barra3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.button1.CausesValidation = false;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(201, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 45);
+            this.button1.TabIndex = 28;
+            this.button1.TabStop = false;
+            this.button1.Text = "Stretch";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Candara Light", 15.75F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(196, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 26);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "How to use:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(14, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(446, 17);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "1 - Make sure that in Valorant the \"Aspect Ratio Method\" option is set to \"Fill\"." +
+    "";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Leelawadee UI Semilight", 9.75F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(14, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 17);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "2 - Change your resolution in windows Config";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Leelawadee UI Semilight", 9.75F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(14, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(394, 17);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "3 - Press the \"Stretch\" button and wait for the game to go full screen";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(42, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(533, 319);
+            this.panel1.TabIndex = 38;
+            // 
+            // barra3
+            // 
+            this.barra3.BackColor = System.Drawing.Color.White;
+            this.barra3.Location = new System.Drawing.Point(42, 358);
+            this.barra3.Name = "barra3";
+            this.barra3.Size = new System.Drawing.Size(235, 10);
+            this.barra3.TabIndex = 39;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(562, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 41);
+            this.button2.TabIndex = 40;
+            this.button2.TabStop = false;
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Valostretch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(603, 407);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.barra3);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Valostretch";
+            this.Text = "Valostretch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel barra3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ValorantEsticado/Valostretch.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Drawing.Drawing2D;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+
+namespace ValorantEsticado
+{
+    public partial class Valostretch : Form
+    {
+        private const int GWL_STYLE = -16;
+        private const int WS_BORDER = 0x00800000;
+        private const int WS_POPUP = unchecked((int)0x80000000);
+        private const int WS_VISIBLE = 0x10000000;
+        private const uint SWP_FRAMECHANGED = 0x0020;
+        private const string ProcessName = "VALORANT-Win64-Shipping";
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        public Valostretch()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 10, 10));
+
+
+            barra3.Size = new Size(532, 3);
+
+        }
+
+        private void SetValorantFullscreen()
+        {
+            IntPtr windowHandle = GetWindowHandleByProcessName(ProcessName);
+
+            if (windowHandle != IntPtr.Zero)
+            {
+                SetWindowLong(windowHandle, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+                SetWindowPos(windowHandle, IntPtr.Zero, 0, 0, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, SWP_FRAMECHANGED);
+            }
+            else
+            {
+                Console.WriteLine("Processo não encontrado.");
+            }
+        }
+
+        private void RemoveBorder()
+        {
+            IntPtr windowHandle = GetWindowHandleByProcessName(ProcessName);
+
+            if (windowHandle != IntPtr.Zero)
+            {
+                RECT rect;
+                GetWindowRect(windowHandle, out rect);
+
+                int style = GetWindowLong(windowHandle, GWL_STYLE);
+                style &= ~WS_BORDER;
+                SetWindowLong(windowHandle, GWL_STYLE, style);
+                SetWindowPos(windowHandle, IntPtr.Zero, rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top, SWP_FRAMECHANGED);
+            }
+            else
+            {
+                Console.WriteLine("Processo não encontrado.");
+            }
+        }
+
+        private IntPtr GetWindowHandleByProcessName(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            if (processes.Length > 0)
+            {
+                return processes[0].MainWindowHandle;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int alpha = 75;
+            int red = (int)(Math.Sin(Environment.TickCount * 0.002) * 127 + 128);
+            int green = (int)(Math.Sin(Environment.TickCount * 0.002 + 2) * 127 + 128);
+            int blue = (int)(Math.Sin(Environment.TickCount * 0.002 + 4) * 127 + 128);
+
+            button1.ForeColor = Color.FromArgb(alpha, red, green, blue); 
+            label3.ForeColor = Color.FromArgb(alpha, red, green, blue);
+            barra3.BackColor = Color.FromArgb(alpha, red, green, blue);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            RemoveBorder();
+            Thread.Sleep(1000);
+            SetValorantFullscreen();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/realkayo/Valostretch/tree/main");
+        }
+    }
+}
